377db8ba5b4dfc39a1cbade9f8043739bacb55f2
Engranajes-MEDAC <info@engranajesciencia.com>
2025-11-19 12:16:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
+// vite.config.js
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // *** RUTA BASE CONFIGURADA PARA GITHUB PAGES ***
+  // Anterior: '/04-WebApp-Prehistoria/'
+  // Nuevo: '/Parque-Prehistoria/' (Coincide con el nombre del repositorio)
+  base: '/Parque-Prehistoria/', 
+  // ***********************************************
   plugins: [react()],
-  base: '/04-WebApp-Prehistoria/',
   server: {
     fs: {
       // 🔐 Reforzar seguridad: evitar que se lean archivos fuera del directorio del proyecto
