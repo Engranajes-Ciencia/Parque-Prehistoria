1fc546ba6f788d209f4a9b8a792171c4b30f05af
Engranajes-MEDAC <info@engranajesciencia.com>
2025-11-23 14:46:30 +0100

    Segunda correccion de errores

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Parque-Prehistoria/',
+  base: './',
   server: {
     fs: {
       // 🔐 Reforzar seguridad: evitar que se lean archivos fuera del directorio del proyecto
